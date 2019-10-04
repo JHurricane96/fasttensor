b6e1429db332e6c92e1f96d68fbd167c96315012
Arun Ramachandran <arun.jhurricane@gmail.com>
2019-10-04 18:30:54 +0000

    Update benchmark with eager eval

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -10,8 +10,8 @@
 using namespace std;
 using namespace fasttensor;
 
-const int TRIES = 2;
-const int REPEAT = 10;
+const int TRIES = 10;
+const int REPEAT = 100;
 
 template <typename ElementType, int Rank>
 Tensor<ElementType, Rank> make_rand_tensor(array<ptrdiff_t, Rank> dimensions) {
@@ -47,8 +47,26 @@ void print_results(string test_name, const BenchTimer &timer, double flops_facto
             << " GFlops\n";
 }
 
+template <typename Result, typename Operand>
+inline void _eager_eval(Result &result, Operand &operand) {
+  result = result + operand;
+}
+
+template <typename Result, typename Operand, typename... OtherOperands>
+inline void _eager_eval(Result &result, Operand &operand, OtherOperands &... other_operands) {
+  result = result + operand;
+  _eager_eval(result, other_operands...);
+}
+
+template <typename Result, typename Operand1, typename Operand2, typename... OtherOperands>
+inline void eager_eval(Result &result, Operand1 &operand1, Operand2 &operand2,
+                       OtherOperands &... other_operands) {
+  result = operand1 + operand2;
+  _eager_eval(result, other_operands...);
+}
+
 int main() {
-  ptrdiff_t row = 1E4, col = 1E3, dep = 10;
+  ptrdiff_t row = 1E4, col = 1E2, dep = 1E1;
   array<ptrdiff_t, 3> dimensions{row, col, dep};
   auto a = make_rand_tensor<float, 3>(dimensions);
   auto b = make_rand_tensor<float, 3>(dimensions);
@@ -60,6 +78,10 @@ int main() {
 
   BENCH(timer, TRIES, REPEAT, result = a + b + c + d);
   print_results("a+b+c+d (Lazy)", timer, double(row * col * dep * 3));
+  std::cout << result.getCoeff(0) << "\n";
+
+  BENCH(timer, TRIES, REPEAT, eager_eval(result, a, b, c, d));
+  print_results("a+b+c+d (Eager)", timer, double(row * col * dep * 3));
   std::cout << result.getCoeff(0) << "\n";
 
   return 0;
