6cb57022b4605fce083a95f334116df7547a6f68
Arun Ramachandran <arun.jhurricane@gmail.com>
2019-11-06 14:25:53 +0000

    Add multi-GPU support
    Work is split as equally as possible among multiple GPUs if they are
    present

--- fasttensor/Assign.hpp
+++ fasttensor/Assign.hpp
@@ -36,10 +36,10 @@ inline void Assign(Tensor<ElementType, Rank, DefaultDevice> &lhs, OtherExpr cons
 #elif defined FASTTENSOR_GPU
 
 template <typename ElementType, typename OtherExpr, typename = enable_if_tensor_exprs<OtherExpr>>
-__global__ void Kernel(ElementType *lhs_storage, OtherExpr rhs, int num_elements) {
-  int index = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void Kernel(ElementType *lhs_storage, OtherExpr rhs, int start_offset, int end_offset) {
+  int index = blockIdx.x * blockDim.x + threadIdx.x + start_offset;
   int stride = blockDim.x * gridDim.x;
-  for (int i = index; i < num_elements; i += stride) {
+  for (int i = index; i < end_offset; i += stride) {
     lhs_storage[i] = rhs.getCoeff(i);
   }
 }
@@ -49,23 +49,49 @@ template <typename ElementType, int Rank, typename OtherExpr,
 inline void Assign(Tensor<ElementType, Rank, GpuDevice> &lhs, OtherExpr const &rhs) {
   auto unwrapped_rhs = UnwrapStorage(rhs);
 
-  auto &device_props = lhs.device().deviceProps()[0];
-  auto block_size = device_props.blockSize();
-  auto max_blocks = device_props.maxBlocks();
-
+  auto &device_props = lhs.device().deviceProps();
+  auto num_devices = device_props.size();
   auto num_elements = lhs.num_elements();
-  int num_blocks = std::min((long)max_blocks, (num_elements + block_size - 1) / block_size);
+  auto num_elts_per_device_floored = num_elements / num_devices;
+  auto partition_point = num_devices - (num_elements % num_devices);
 
-  Kernel<<<num_blocks, block_size>>>(lhs.storage().elements(), unwrapped_rhs, num_elements);
-  cudaDeviceSynchronize();
+  auto num_calculted_elts = 0;
+  for (int i = 0; i < num_devices; ++i) {
+    cudaSetDevice(i);
+
+    auto &device = device_props[i];
+    auto block_size = device.blockSize();
+    auto max_blocks = device.maxBlocks();
+    decltype(num_elements) num_elts_current_device = 0;
+
+    if (i >= partition_point) {
+      num_elts_current_device = num_elts_per_device_floored + 1;
+    } else {
+      num_elts_current_device = num_elts_per_device_floored;
+    }
+
+    int num_blocks = std::min((decltype(num_elements))max_blocks,
+                              (num_elts_current_device + block_size - 1) / block_size);
+    auto end_offset = num_calculted_elts + num_elts_current_device;
+
+    Kernel<<<num_blocks, block_size>>>(lhs.storage().elements(), unwrapped_rhs, num_calculted_elts,
+                                       end_offset);
+
+    num_calculted_elts = end_offset;
+  }
+
+  for (int i = 0; i < device_props.size(); ++i) {
+    cudaSetDevice(i);
+    cudaDeviceSynchronize();
+  }
 }
 
 #endif
 
 template <typename ElementType, int Rank, typename DeviceType>
 template <typename OtherExpr, typename>
-inline Tensor<ElementType, Rank, DeviceType> &Tensor<ElementType, Rank, DeviceType>::
-operator=(OtherExpr const &other) {
+inline Tensor<ElementType, Rank, DeviceType> &
+Tensor<ElementType, Rank, DeviceType>::operator=(OtherExpr const &other) {
   Assign(*this, other);
   return *this;
 }
